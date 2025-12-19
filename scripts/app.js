4d3b77636bb2877b6985ba7e7ea7b9e33f5934b9
NotWhoops <whoopsplayz@gmail.com>
2025-12-19 12:07:51 -0800

    removed pages

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,9 @@
 import { API_KEY } from './environment.js';
 
+function getWeatherData(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=952e7cec2cadaedaa7f4d6e715973a12')
+      .then(response => response.json())
+      .then(json => console.log(json))
+}
+
+getWeatherData();
